5a3d5bd5bfd6653c9140c4ba78918da525ffe07d
ajikcey <ajikcey@mail.ru>
2014-03-20 08:56:59 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(function() {
   $('button').on('click', function() {
-    var form = $(this).parents('form');
+    var form = $(this).parents('#form');
     var name = form.find('input[name="name"]');
     name.parent().find('div').html(name.val());
     var password = form.find('input[name="password"]');
